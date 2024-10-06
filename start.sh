d3bc8713906be1dcfd32ab57814f027c7804b0f3
saeedmaghdam <smasafat@gmail.com>
2024-10-06 03:11:22 +0200

    add start.sh

---
+++ start.sh
@@ -0,0 +1,7 @@
+wget https://raw.githubusercontent.com/saeedmaghdam/k8s/refs/heads/main/setup-alpine
+mv setup-alpine setup-alpine.sh
+chmod +x setup-alpine.sh
+
+wget https://raw.githubusercontent.com/saeedmaghdam/k8s/refs/heads/main/config-alpine
+mv config-alpine config-alpine.sh
+chmod +x config-alpine.sh
