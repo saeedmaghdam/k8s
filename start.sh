2050a7f20cd1d4ad36b1661d1878d95b7d0bf831
saeedmaghdam <smasafat@gmail.com>
2024-10-06 03:17:34 +0200

    split config into two files

--- start.sh
+++ start.sh
@@ -1,7 +1,11 @@
-wget https://raw.githubusercontent.com/saeedmaghdam/k8s/refs/heads/main/setup-alpine
-mv setup-alpine 1-setup-alpine.sh
+wget https://raw.githubusercontent.com/saeedmaghdam/k8s/refs/heads/main/1-setup-alpine
+mv 1-setup-alpine 1-setup-alpine.sh
 chmod +x 1-setup-alpine.sh
 
-wget https://raw.githubusercontent.com/saeedmaghdam/k8s/refs/heads/main/config-alpine
-mv config-alpine 2-config-alpine.sh
+wget https://raw.githubusercontent.com/saeedmaghdam/k8s/refs/heads/main/2-config-alpine
+mv 2-config-alpine 2-config-alpine.sh
 chmod +x 2-config-alpine.sh
+
+wget https://raw.githubusercontent.com/saeedmaghdam/k8s/refs/heads/main/3-get-config
+mv 3-get-config 3-get-config.sh
+chmod +x 3-get-config.sh
